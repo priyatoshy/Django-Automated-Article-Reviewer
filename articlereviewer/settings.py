caf666490e800a4d1c82eafb481403096c00b9b6
Priyatosh Satpati <97145555+priyatoshy@users.noreply.github.com>
2022-12-10 18:23:34 +0530

    Update settings.py

--- articlereviewer/settings.py
+++ articlereviewer/settings.py
@@ -117,8 +117,8 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 
-EMAIL_HOST_USER='priyatoshsatpatitest@gmail.com'
-EMAIL_HOST_PASSWORD='bhgjkckkcslqzuzq'
+EMAIL_HOST_USER='#############'
+EMAIL_HOST_PASSWORD='######################'
 
 
 
